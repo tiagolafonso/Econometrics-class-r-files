ca43f608b460225198e257c41c08d2602fd70cdb
Tiago Afonso <149522530+tiagolafonso@users.noreply.github.com>
2024-12-12 10:04:50 +0000

    Update Class20.r

--- Class20_ex_r/Class20.r
+++ Class20_ex_r/Class20.r
@@ -32,13 +32,13 @@ library(modelsummary)
 
 #load data htv file
 htv <- read.csv("htv.csv")
-head(htv) #show an overview of the data
+head(htv)
 
 #i)
 model_wage <- lm(log(wage)~educ+abil+exper+nc+west+south+urban, data=htv)
 summary(model_wage)
 
-#ii) filtro educ<16
+#ii) 
 htv_sub <- dplyr::filter(htv,educ<16)
 model_wage_sub <- lm(log(wage)~educ+abil+exper+nc+west+south+urban, data=htv_sub)
 model_wage_sub <- lm(log(wage)~educ+abil+exper+nc+west+south+urban,subset = (educ<16), data=htv)
@@ -46,7 +46,7 @@ model_wage_sub <- lm(log(wage)~educ+abil+exper+nc+west+south+urban,subset = (edu
 stargazer(model_wage,model_wage_sub,type="text")
 modelsummary(list(model_wage,model_wage_sub))
 
-#iii) educ<16 e wage<20
+#iii) 
 htv_sub2 <- dplyr::filter(htv,educ<16 & wage<20)
 htv_sub3<-filter(htv_sub, wage<20)
 model_wage_sub2 <- lm(log(wage)~educ+abil+exper+nc+west+south+urban, data=htv_sub2)
@@ -60,12 +60,12 @@ modelsummary(list(model_wage,model_wage_sub2))
 #carregar dados happiness
 happiness<- read_xlsx("happiness.xlsx")
 
-# estimar probit
+#
 model_happiness <- glm(vhappy~occattend+regattend, family=binomial(link="probit"), data=happiness)
-# average partial effects probit
+# 
 probitmfx(model_happiness, atmean=FALSE, data=happiness)
 
-#lpm version
+#
 lpm_happiness2 <- lm(vhappy~occattend+regattend, data=happiness)
 summary(lpm_happiness2)
 
@@ -88,21 +88,21 @@ table(alcohol$abuse)
 974/(8848+974)
 
 #ii)
-#modelo lpm
+
 model_alcohol_lpm <- lm(employ~abuse, data=alcohol)
-?lm
+
 coeftest(model_alcohol_lpm, vcov=vcovHAC(model_alcohol_lpm, type="HC1"))
 
 #iii)
 
-#modelo probit
+
 model_alcohol_probit <- glm(employ~abuse, family=binomial(link="probit"), data=alcohol)
-#average partial effects
+
 probitmfx(model_alcohol_probit, atmean=FALSE, data=alcohol)
 summary(model_alcohol_lpm)
 
 # iv)
-# fitted lpm e probit
+
 alcohol$fitted_lpm <- fitted(model_alcohol_lpm)
 alcohol$fitted_probit <- fitted(model_alcohol_probit)
 
@@ -114,15 +114,15 @@ model_alcohol_lpm_full <- lm(employ~abuse+age+agesq+educ+
                       midwest+south+centcity+outercity+qrt1+
                       qrt2+qrt3, data=alcohol)
 
-#comparar modelos
+
 stargazer(model_alcohol_lpm,model_alcohol_lpm_full, type="text")
 modelsummary(list(model_alcohol_lpm,model_alcohol_lpm_full))
 
 #vi)
-#modelo probit
+
 model_alcohol_probit_full <- glm(employ~abuse+age+agesq+educ+
                       educsq+married+famsize+northeast+
                       midwest+south+centcity+outercity+qrt1+
                       qrt2+qrt3, family=binomial(link="probit"), data=alcohol)
-# ape_probit
+
 probitmfx(model_alcohol_probit_full, atmean=FALSE, data=alcohol)
